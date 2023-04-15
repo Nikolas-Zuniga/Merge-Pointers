796c5b45ec30e4a5c436b05e65c51335d569728f
Nikolas-Zuniga <129298541+Nikolas-Zuniga@users.noreply.github.com>
2023-04-15 09:54:35 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int A[12] = { 2,4,10,24,32,34,1,9,17,31,33,129 };
+int A[12] = {2, 4, 10, 24, 32, 34, 1, 9, 17, 31, 33, 129};
 
-void prin() {
-    cout << "| ";
-    for (int i{ 0 }; i < size(A); i++) {
-        cout << A[i] << " | ";
-    }
-    cout << "\n";
+void prinPir() {
+  cout << "| ";
+  for (int *i = A; i < A + (sizeof(A) / sizeof(A[0])); i++) {
+    cout << *i << "| ";
+  }
+  cout << "\n";
 }
 
-void merge(int* p, int* q) {
+void merge(int *p, int *q) {
 
-    int aux{ 0 };
-    int* end = q + (q - p);
-    for (; q < end; q++) {
+  int aux{0};
+  int *end = q + (q - p);
 
-        for (; p < q; p++) {
-
-            if (*p > *q) {
-                aux = *p;
-                *p = *q;
-                *q = aux;
-
-                for (int* r = p + 1; r < q; r++) {
-                    aux = *r;
-                    *r = *q;
-                    *q = aux;
-                }
-            }
-        }
-        p = p - (end - q);
+  while (p < q && q < end) {
+    if (*p < *q) {
+      p++;
+    } else {
+      for (int *r = p; r < q; r++) {
+        aux = *r;
+        *r = *q;
+        *q = aux;
+      }
+      p++;
+      q++;
     }
+  }
 }
 
-int main()
-{
-    prin();
-    merge(A, A + (sizeof(A) / sizeof(A[0])) / 2);
-    prin();
+int main() {
+  prinPir();
+  merge(A, A + (sizeof(A) / sizeof(A[0])) / 2);
+  prinPir();
 }
